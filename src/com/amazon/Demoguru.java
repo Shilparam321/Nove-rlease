311816d3b166e6c5857a6f21563618a148f87841
shilpa <shilparam@gmail.com>
2023-11-17 13:01:06 +0530

    my status

--- src/com/amazon/Demoguru.java
+++ src/com/amazon/Demoguru.java
@@ -5,5 +5,7 @@ public void sabina()
 {
 	System.out.println("Need to be change");
 	System.out.println("what is you name");
+	System.out.println("i am too good");
+	System.out.println(" i ver nice");
 }
 }
